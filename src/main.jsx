bc5fe44504ca6f7e88dd3e23b9be225def4d0735
Azula100 <zulchimegzula@gmail.com>
2026-06-10 11:17:14 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import axios from 'axios'
 import App from './App.jsx'
 import './styles/style.css'
-axios.defaults.baseURL = 'http://localhost:4000'
+axios.defaults.baseURL = 'https://news-backend.onrender.com'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
